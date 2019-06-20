45012707bbb5019f4856a0b9763f88468013d986
Seblet77 <seb.lethuaire@hotmail.fr>
2019-06-20 10:05:49 +0200

    requetes ajoutees

--- Analysis.py
+++ Analysis.py
@@ -4,66 +4,60 @@
 #
 # Author:      Sébastien
 #
-# Created:     12/06/2019
+# Created:     18/06/2019
 # Copyright:   (c) Sébastien 2019
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
 import pandas
-import csv
+
 from tkinter import *
 
-
-class Checkbar(Frame):
-   def __init__(self, parent=None, picks=[], side=LEFT, anchor=W):
-      Frame.__init__(self, parent)
-      self.vars = []
-      for pick in picks:
-         var = IntVar()
-         chk = Checkbutton(self, text=pick, variable=var)
-         chk.pack(side=side, anchor=anchor, expand=YES)
-         self.vars.append(var)
-   def state(self):
-      return map((lambda var: var.get()), self.vars)
+def main():
+    pass
 
 if __name__ == '__main__':
-   root = Tk()
-   lng = Checkbar(root, ['2012', '2013', '2014', '2015'])
-   lng.pack(side=TOP,  fill=X)
-   lng.config(relief=GROOVE, bd=2)
-
-   def allstates():
-      print(list(lng.state()))
-
-   Button(root, text='Quit', command=root.destroy).pack(side=RIGHT)
-   Button(root, text='Peek', command=allstates).pack(side=RIGHT)
-
-   root.mainloop()
+    main()
 
 
+df=pandas.read_csv("dans-ma-rue.csv", sep='\t', delimiter=';', header=0)
+
+fenetre = Tk()
+
+canvas = Canvas(fenetre, width=800, height=450, bg='ivory')
+
+def graphique1():
+    canvas.delete("all")
+    resultat1=df.groupby(['ARRONDISSEMENT','ANNEE DECLARATION']).size()
+    print(resultat1)
 
 
+def graphique2():
+    canvas.delete("all")
+    resultat2=df.groupby(['TYPE','MOIS DECLARATION']).size()
+    print(resultat2)
 
 
+def graphique3():
+    canvas.delete("all")
+    resultat3=df.groupby(['TYPE','ARRONDISSEMENT']).size()
+    print(resultat3)
 
 
+def initBoutons():
+    bouton1 = Button(fenetre, text="Question 1", command=graphique1)
+    bouton1.pack(padx = 5, pady = 5)
+    bouton2 = Button(fenetre, text="Question 2", command=graphique2)
+    bouton2.pack(padx = 5, pady = 5)
+    bouton3 = Button(fenetre, text="Question 3", command=graphique3)
+    bouton3.pack(padx = 5, pady = 5)
+
+    bouton0 = Button(fenetre, text="Quitter", command=fenetre.destroy)
+    bouton0.pack(padx = 5, pady = 5)
 
 
+canvas.pack(side=RIGHT, padx=3, pady=3)
 
+initBoutons()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fenetre.mainloop()
